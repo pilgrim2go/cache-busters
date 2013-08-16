6e32b121aaa45c02b604b04954b028b8bfde1ac1
Alex Gaynor <alex.gaynor@gmail.com>
2013-08-16 14:05:49 -0700

    Tests and implementation for a failure to delete a cache key

--- cache_buster/driver.py
+++ cache_buster/driver.py
@@ -14,16 +14,23 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from twisted.internet.defer import gatherResults
+
+
 class Driver(object):
     """
     :attr _fkt: A FormattingKeyThingy.
     :attr _cache: A CacheDeleter.
+    :attr _logger: A logger.
     """
 
-    def __init__(self, fkt, cache):
+    def __init__(self, fkt, cache, logger):
         self._fkt = fkt
         self._cache = cache
+        self._logger = logger
 
     def invalidate_row(self, table, row):
-        for key in self._fkt.keys_for_row(table, row):
-            self._cache.delete(key)
+        return gatherResults([
+            self._cache.delete(key).addErrback(self._logger.err, table, key)
+            for key in self._fkt.keys_for_row(table, row)
+        ]).addCallback(lambda _: None)
--- cache_buster/test/test_driver.py
+++ cache_buster/test/test_driver.py
@@ -16,16 +16,18 @@ limitations under the License.
 
 import pretend
 
-from twisted.internet.defer import succeed
+from twisted.internet.defer import Deferred, succeed, fail
+from twisted.python.failure import Failure
 from twisted.trial import unittest
 
 from cache_buster.driver import Driver
 from cache_buster.keys import FormattingKeyThingy
+from cache_buster.test.doubles import DummyLogger
 
 
 class DriverTests(unittest.TestCase):
     def test_construct(self):
-        Driver(FormattingKeyThingy({}), None)
+        Driver(FormattingKeyThingy({}), None, None)
 
     def test_invalidate_row_calls_cache_delete(self):
         cache = pretend.stub(
@@ -33,6 +35,50 @@ class DriverTests(unittest.TestCase):
         )
         d = Driver(FormattingKeyThingy({
             "foo_table": ["bar", "baz"]
-        }), cache)
+        }), cache, DummyLogger())
         d.invalidate_row("foo_table", {})
         self.assertEqual(cache.delete.calls, [pretend.call("bar"), pretend.call("baz")])
+
+    def test_invalidate_row_returns_deferred(self):
+        d = Driver(FormattingKeyThingy({}), None, DummyLogger())
+        res = self.successResultOf(d.invalidate_row("foo_table", {}))
+        self.assertIs(res, None)
+
+    def test_invalidate_row_returns_deferred_fired_when_cache_delete_fires(self):
+        d1 = Deferred()
+        cache = pretend.stub(
+            delete=lambda key: d1,
+        )
+        d = Driver(FormattingKeyThingy({
+            "foo_table": ["bar"]
+        }), cache, DummyLogger())
+        invalidate_d = d.invalidate_row("foo_table", {})
+        self.assertNoResult(invalidate_d)
+        d1.callback(None)
+        res = self.successResultOf(invalidate_d)
+        self.assertIs(res, None)
+
+    def test_invalidate_row_succeeds_on_cache_delete_failure(self):
+        cache = pretend.stub(
+            delete=lambda key: fail(Exception()),
+        )
+        d = Driver(FormattingKeyThingy({
+            "foo_table": ["bar"]
+        }), cache, DummyLogger())
+        invalidate_d = d.invalidate_row("foo_table", {})
+        res = self.successResultOf(invalidate_d)
+        self.assertIs(res, None)
+
+    def test_invalidate_row_logs_on_cache_delete_failure(self):
+        f = Failure(Exception())
+        cache = pretend.stub(
+            delete=lambda key: fail(f),
+        )
+        logger = pretend.stub(
+            err=pretend.call_recorder(lambda failure, table, key: None)
+        )
+        d = Driver(FormattingKeyThingy({
+            "foo_table": ["bar"]
+        }), cache, logger)
+        d.invalidate_row("foo_table", {})
+        self.assertEqual(logger.err.calls, [pretend.call(f, "foo_table", "bar")])
